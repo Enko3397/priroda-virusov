b3945cd638d019460ebeffa2ab39a07724eed4b4
Enko <116001719+Enko3397@users.noreply.github.com>
2024-12-13 20:41:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Функция для переключения темы
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const body = document.body;
+
+themeToggleBtn.addEventListener('click', () => {
+  body.classList.toggle('light-theme');
+  if (body.classList.contains('light-theme')) {
+    themeToggleBtn.textContent = 'Переключить на темную тему';
+  } else {
+    themeToggleBtn.textContent = 'Переключить на светлую тему';
+  }
+});
+
+// Добавление обработчиков событий для кнопок
 document.addEventListener("DOMContentLoaded", () => {
-  // Обработчики для кнопок
   const actionBtns = document.querySelectorAll('.action-btn');
   actionBtns.forEach(btn => {
     btn.addEventListener('click', () => {
